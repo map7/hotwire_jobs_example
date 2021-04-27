0cf991eaa98fbdb424b5a912f11f8e0659dd1068
Michael Pope <michael@dtcorp.com.au>
2021-04-27 15:10:37 +1000

    Jobs-Allow delete to work without refresh

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -52,6 +52,7 @@ class JobsController < ApplicationController
   def destroy
     @job.destroy
     respond_to do |format|
+      format.turbo_stream
       format.html { redirect_to jobs_url, notice: "Job was successfully destroyed." }
       format.json { head :no_content }
     end
