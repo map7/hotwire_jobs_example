ba777c836e194e30e1e02a02d4fe3614ef17283e
Michael Pope <michael@dtcorp.com.au>
2021-04-28 13:10:52 +1000

    Jobs-FEAT-Add target to broadcast

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,6 @@ class Job < ApplicationRecord
   # We're going to publish to the stream :jobs
   # It'll update the element with the ID of the dom_id for this object,
   # Or append our jobs/_job partial, to the #jobs <tbody>
-  broadcasts_to -> (job) {:jobs}
-  
+
+  broadcasts_to -> (job) {:jobs}, target: :jobs
 end
